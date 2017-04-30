7e74472bfeec2343303bd1ae51b2daec7195a54a
CristhianCV <cristhiancv45@gmail.com>
2017-04-30 01:39:49 -0500

    Update MainPage.xaml.cs

--- GRLLReportApp/GRLLReportApp/MainPage.xaml.cs
+++ GRLLReportApp/GRLLReportApp/MainPage.xaml.cs
@@ -12,6 +12,8 @@ namespace GRLLReportApp
         public MainPage()
         {
             InitializeComponent();
+            string html = "https://app.powerbi.com/view?r=eyJrIjoiNWFlN2Q3ODctOWVmYS00YmJiLTljMTUtMTc5ZDRjM2U1YTM4IiwidCI6IjY1MTgxYWE4LTI5NTgtNDE2ZC05MWEwLTEwODAzMzE4NWUxZiIsImMiOjR9&dynamicDisplayOption=true";
+            Browser.Source = html;
         }
     }
 }
